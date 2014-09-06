9630f5625572267d973ed11bc096d4fcee29fa1d
Max Yurlov <maxyurloff@gmail.com>
2014-09-06 22:48:11 +0300

    ticket:3 - done

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -5,9 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'basic.views.index', name='home'),
-    # url(r'^$', 'fortytwo_test_task.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^', include('basic.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
